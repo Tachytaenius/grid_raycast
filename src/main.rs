c11c97ccce9085843cf3a2780fa07f795d43d416
Tachytaenius <henryfthomson@gmail.com>
2023-05-18 17:40:20 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,176 @@
+// LEAVE NO TODOS! Check both files!
+
+// TODO: Test for special cases, including rays on the grid lines
+// TODO: Figure out raycasts starting on a tile edge
+// TODO: Remove the print statements
+
+mod raycast;
+
+use raycast::*;
+
+use bevy::prelude::*;
+use bevy_prototype_lyon::prelude::*;
+
+const GRID_WIDTH: i32 = 20;
+const GRID_HEIGHT: i32 = 18;
+const TILE_WIDTH: f32 = 30.5;
+const TILE_HEIGHT: f32 = TILE_WIDTH; // TODO: Varied!
+const GRID_OFFSET_X: f32 = 10.0;
+const GRID_OFFSET_Y: f32 = 10.0;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(ShapePlugin)
+
+        .insert_resource(ClearColor(Color::BLACK))
+        .insert_resource(RayPoints {..default()})
+
+        .add_startup_systems((
+            create_grid,
+            create_camera,
+            create_ray
+        ))
+
+        .add_systems((
+            update_ray,
+            update_raycast
+        ).chain())
+
+        .run();
+}
+
+#[derive(Resource, Default)]
+struct RayPoints { // More like a line segment with direction information
+    start: Vec2,
+    end: Vec2
+}
+
+#[derive(Component)]
+struct RayDisplay;
+
+#[derive(Component)]
+struct IntersectionPoint;
+
+fn create_ray(
+    mut commands: Commands
+) {
+    commands.spawn((
+        ShapeBundle {..default()},
+        Fill::color(Color::YELLOW),
+        Stroke::new(Color::YELLOW, 1.0),
+        RayDisplay
+    ));
+}
+
+fn create_camera(
+    mut commands: Commands,
+    window_query: Query<&Window, With<bevy::window::PrimaryWindow>>
+) {
+    let window = window_query.get_single().unwrap();
+    commands.spawn(Camera2dBundle {
+        transform: Transform {
+            translation: Vec3::new(
+                window.width() / 2.0,
+                window.height() / 2.0,
+                999.9
+            ),
+            ..default()
+        },
+        ..default()
+    });
+}
+
+fn create_grid(
+    mut commands: Commands
+) {
+    let offset_vector = Vec2::new(GRID_OFFSET_X, GRID_OFFSET_Y);
+    let scale_vector = Vec2::new(TILE_WIDTH, TILE_HEIGHT);
+
+    for x in 0..=GRID_WIDTH {
+        let x = x as f32;
+        let line = shapes::Line(
+            Vec2::new(x, 0.0) * scale_vector + offset_vector,
+            Vec2::new(x, GRID_HEIGHT as f32) * scale_vector + offset_vector
+        );
+        commands.spawn((
+            ShapeBundle {
+                path: GeometryBuilder::build_as(&line),
+                ..default()
+            },
+            Fill::color(Color::GRAY),
+            Stroke::new(Color::GRAY, 1.0),
+        ));
+    }
+
+    for y in 0..=GRID_HEIGHT {
+        let y = y as f32;
+        let line = shapes::Line(
+            Vec2::new(0.0, y) * scale_vector + offset_vector,
+            Vec2::new(GRID_WIDTH as f32, y) * scale_vector + offset_vector
+        );
+        commands.spawn((
+            ShapeBundle {
+                path: GeometryBuilder::build_as(&line),
+                ..default()
+            },
+            Fill::color(Color::GRAY),
+            Stroke::new(Color::GRAY, 1.0),
+        ));
+    }
+}
+
+fn update_ray(
+    mut commands: Commands,
+    mut ray: ResMut<RayPoints>,
+    window_query: Query<&Window, With<bevy::window::PrimaryWindow>>,
+    mouse_buttons: Res<Input<MouseButton>>,
+    mut ray_query: Query<Entity, With<RayDisplay>>
+) {
+    let window = window_query.get_single().unwrap();
+    if let Some(mouse_position) = window.cursor_position() {
+        if mouse_buttons.pressed(MouseButton::Left) {
+            ray.start = mouse_position;
+        }
+
+        ray.end = mouse_position;
+        if mouse_buttons.pressed(MouseButton::Right) {
+            // ray.end = ray.start;
+
+            // TEMP:
+            ray.start = Vec2::new(40.0, 45.0)
+        }
+    }
+
+    // Decided to compress this into a one-liner just for fun
+    commands.entity(ray_query.get_single_mut().unwrap()).insert(GeometryBuilder::build_as(&shapes::Line(ray.start, ray.end)));
+}
+
+fn update_raycast(
+    mut commands: Commands,
+    ray: Res<RayPoints>,
+    old_circle_query: Query<Entity, With<IntersectionPoint>>
+) {
+    for entity in old_circle_query.iter() {
+        commands.entity(entity).despawn();
+    }
+
+    let raycast = line_tilemap_intersections_iterator_struct(ray.start, ray.end, TILE_WIDTH, Vec2::new(GRID_OFFSET_X, GRID_OFFSET_Y));
+    for raycast_result in raycast {
+        // TODO: Mark tiles
+        println!("{}", raycast_result.intersection_t);
+        commands.spawn((
+            ShapeBundle {
+                path: GeometryBuilder::build_as(&shapes::Circle {
+                    center: ray.start.lerp(ray.end, raycast_result.intersection_t),
+                    radius: 2.0
+                }),
+                ..default()
+            },
+            Fill::color(Color::RED),
+            Stroke::new(Color::RED, 1.0),
+            IntersectionPoint
+        ));
+    }
+    println!("");
+}
---
+++ src/raycast.rs
@@ -0,0 +1,128 @@
+use bevy::prelude::*;
+
+pub struct GridRaycast {
+    tile_x: i32,
+    tile_y: i32,
+    delta_tile_x: i32,
+    delta_tile_y: i32,
+    delta_t_x: f32,
+    delta_t_y: f32,
+    delta_delta_t_x: f32,
+    delta_delta_t_y: f32,
+    t: f32,
+    last_t: Option<f32>,
+
+    ray_start_x: f32, // Relative to the frame where the grid offset is 0
+    ray_start_y: f32,
+    ray_end_x: f32,
+    ray_end_y: f32
+}
+
+pub struct GridRaycastResult {
+    pub tile_x: i32,
+    pub tile_y: i32,
+    pub intersection_t: f32
+}
+
+impl Iterator for GridRaycast {
+    type Item = GridRaycastResult;
+
+    fn next(&mut self) -> Option<GridRaycastResult> {
+        if
+            self.ray_start_x == self.ray_end_x &&
+            self.ray_start_y == self.ray_end_y
+        { // TODO: Implement a good way to not do this check every iteration
+            if self.last_t.is_some() {
+                return None;
+            } else {
+                debug_assert!(self.t == 0.0); // Should be undefined, really
+                self.last_t = Some(self.t);
+                return Some(GridRaycastResult {
+                    tile_x: self.tile_x,
+                    tile_y: self.tile_y,
+                    intersection_t: self.t
+                });
+            }
+        }
+
+        // This loop is only here for skipping iterations that have the same t as the previous one
+        loop {
+            if self.t > 1.0 {
+                return None;
+            }
+
+            let result = GridRaycastResult {
+                tile_x: self.tile_x,
+                tile_y: self.tile_y,
+                intersection_t: self.t
+            };
+            self.last_t = Some(self.t);
+
+            if self.delta_t_x < self.delta_t_y {
+                self.tile_x += self.delta_tile_x;
+                let delta_t = self.delta_t_x;
+                self.t += delta_t;
+                self.delta_t_x += self.delta_delta_t_x - delta_t;
+                self.delta_t_y -= delta_t;
+            } else {
+                self.tile_y += self.delta_tile_y;
+                let delta_t = self.delta_t_y;
+                self.t += delta_t;
+                self.delta_t_x -= delta_t;
+                self.delta_t_y += self.delta_delta_t_y - delta_t;
+            }
+
+            if self.t == self.last_t.unwrap() {
+                println!("Skipped");
+                continue;
+            }
+
+            return Some(result);
+        }
+    }
+}
+
+pub fn line_tilemap_intersections_iterator_struct(line_start: Vec2, line_end: Vec2, tile_size: f32, tilemap_position: Vec2) -> GridRaycast {
+    // TODO: independent width and height, probably just needs to give different values to get_helpers
+
+    fn get_helpers(tile_size: f32, pos: f32, dir: f32) -> (i32, i32, f32, f32) {
+        let tile = (pos / tile_size).floor() as i32 + 1; // floor makes a difference for negatives
+
+        let (delta_tile, delta_t);
+        if dir > 0.0 {
+            delta_tile = 1;
+            delta_t = ((tile as f32 + 0.0) * tile_size - pos) / dir;
+        } else if dir == 0.0 {
+            delta_tile = 0;
+            delta_t = ((tile as f32 + 0.0) * tile_size - pos) / dir;
+        } else {
+            delta_tile = -1;
+            delta_t = ((tile as f32 - 1.0) * tile_size - pos) / dir;
+        }
+
+        let delta_delta_t = delta_tile as f32 * tile_size / dir;
+
+        return (tile, delta_tile, delta_t, delta_delta_t);
+    }
+
+    let (tile_x, delta_tile_x, delta_t_x, delta_delta_t_x) = get_helpers(tile_size, line_start.x - tilemap_position.x, (line_end - line_start).x);
+    let (tile_y, delta_tile_y, delta_t_y, delta_delta_t_y) = get_helpers(tile_size, line_start.y - tilemap_position.y, (line_end - line_start).y);
+
+    return GridRaycast {
+        tile_x: tile_x,
+        tile_y: tile_y,
+        delta_tile_x: delta_tile_x,
+        delta_tile_y: delta_tile_y,
+        delta_t_x: delta_t_x,
+        delta_t_y: delta_t_y,
+        delta_delta_t_x: delta_delta_t_x,
+        delta_delta_t_y: delta_delta_t_y,
+        t: 0.0,
+        last_t: None,
+
+        ray_start_x: line_start.x,
+        ray_start_y: line_start.y,
+        ray_end_x: line_end.x,
+        ray_end_y: line_end.y
+    }
+}
